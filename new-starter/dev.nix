e1b0dc4129974766c98f9c6148f7fbe536bd4704
Ryan Lynch <lynch.r.j@gmail.com>
2025-01-05 20:26:07 -0500

    added dev.nix and icon to the build

--- new-starter/dev.nix
+++ new-starter/dev.nix
@@ -19,7 +19,7 @@
       onCreate = {
         npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
         # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "src/App.tsx" "src/App.ts" "src/App.jsx" "src/App.js" ];
+        default.openFiles = [ "README.md" "index.html" ];
       };
       # To run something each time the workspace is (re)started, use the `onStart` hook
     };
--- new-starter/idx-template.nix
+++ new-starter/idx-template.nix
@@ -2,6 +2,8 @@
   packages = [];
   bootstrap = ''
     cp -rf ${./app} "$out"
+    cp -rf ${./dev.nix} "$out/.idx/dev.nix"
+    cp -rf ${./astronaut.png} "$out/.idx/icon.png"
     chmod -R +w "$out"
   '';
 }
