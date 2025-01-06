fcc8d54832fdc635929d4b8ce000d774759e1dc6
Ryan Lynch <lynch.r.j@gmail.com>
2025-01-05 20:42:32 -0500

    working on build script

--- new-starter/idx-template.nix
+++ new-starter/idx-template.nix
@@ -4,11 +4,12 @@
   ];
   bootstrap = ''
     cp -rf ${./app} "$WS_NAME"
+    chmod -R +w "$WS_NAME"
     mkdir -p "$WS_NAME/.idx/"
     cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
     cp -rf ${./astronaut.png} "$WS_NAME/.idx/icon.png"
-    chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
+    chmod -R +w "$WS_NAME"
     cd "$out"; npm install --package-lock-only --ignore-scripts
   '';
 }
