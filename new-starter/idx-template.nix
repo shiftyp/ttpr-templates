dd82bbdf257f0f5a6ae53a7916cbbe5a26a5983f
Ryan Lynch <lynch.r.j@gmail.com>
2025-01-05 20:32:00 -0500

    working on build script

--- new-starter/idx-template.nix
+++ new-starter/idx-template.nix
@@ -4,8 +4,8 @@
     mkdir -p "$WS_NAME"
     cp -rf ${./app} "$WS_NAME"
     mkdir -p "$WS_NAME/.idx/"
-    cp -rf ${./dev.nix} ".idx/dev.nix"
-    cp -rf ${./astronaut.png} ".idx/icon.png"
+    cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
+    cp -rf ${./astronaut.png} "$WS_NAME/.idx/icon.png"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
     cd "$out"; npm install --package-lock-only --ignore-scripts
